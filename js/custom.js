e19799b08af4f52e43d6222ccb7aea5336ff63f2
AbdoAllah Akram <abdoallahakram@gmail.com>
2017-03-13 21:47:08 +0200

    add downCounter to home Page

--- js/custom.js
+++ js/custom.js
@@ -144,12 +144,36 @@ $(function () {
 
     });
     
-    // Display Items 
+    /*--------- Down Counter ----------*/
     
-//    $('.show-more').click(function () {
-//        $('.our-work .hidden').fadeIn(); 
-//        $('.our-work .show-more').fadeOut();
-//    });
+    // Set the date we're counting down to
+    var countDownDate = new Date("april 5, 2017 15:37:25").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
     
     // Adjust MixItUp
 
@@ -188,4 +212,5 @@ $(function () {
             });
         }
     });
+    
 });
